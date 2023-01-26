b7affb7cb0deafa105adeef045607a8aa351bde2
Alexey Zubarev <anzubarev@mts.ru>
2023-01-27 00:47:56 +0300

    specialists, our services

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,7 @@
+let mobile = window.matchMedia('(min-width: 0px) and (max-width: 768px)');
+let tablet = window.matchMedia('(min-width: 769px) and (max-width: 1024px)');
+let desktop = window.matchMedia('(min-width: 1025px)');
+
 function truncateText(dots, truncatedText, readMore) {
     dots.style.display = 'inline';
     truncatedText.style.display = 'none';
@@ -10,23 +14,53 @@ function showText(dots, truncatedText, readMore) {
     readMore.textContent = 'скрыть';
 }
 
-jQuery(function () {
-    const homeSlider = new Swiper('.home-slider .swiper', {
-        loop: true,
-    });
+function initSwiperMobile(swiperInstance, swiperSelector, swiperParams) {
+    if (mobile.matches && !swiperInstance) {
+        swiperInstance = new Swiper(swiperSelector, swiperParams);
+    } else if (tablet.matches && swiperInstance) {
+        swiperInstance.destroy();
+        swiperInstance = null;
+    } else if (desktop.matches && swiperInstance) {
+        swiperInstance.destroy();
+        swiperInstance = null;
+    }
 
+    return swiperInstance;
+}
+
+jQuery(function () {
     const homePromoSlider = new Swiper('.promo-section  .swiper', {
         loop: true,
     });
 
     const specialistsSlider = new Swiper('.specialists-slider .swiper', {
-        loop: true,
-        slidesPerView: 6,
+        loop: false,
+        slidesPerView: 1.3,
+        spaceBetween: 8,
+        breakpoints: {
+            500: {
+                slidesPerView: 5.5,
+                spaceBetween: 10,
+            }
+        }
     });
 
-    const homeServicesSlider = new Swiper('.our-services .services-list .swiper', {
-        loop: true,
-        slidesPerView: 6,
+    let homeServicesSliderParams = {
+        loop: false,
+        slidesPerView: 1.1,
+        spaceBetween: 8,
+        breakpoints: {
+            500: {
+                slidesPerView: 2,
+                spaceBetween: 8,
+            }
+        }
+    };
+    let homeServicesSlider = null;
+    homeServicesSlider = initSwiperMobile(homeServicesSlider, '.our-services .swiper', homeServicesSliderParams);
+
+    $(window).resize(function() {
+        homeServicesSlider = initSwiperMobile(homeServicesSlider, '.our-services .swiper', homeServicesSliderParams);
     });
 
     $('.reviews__item .reviews__item-text').shave(200);
